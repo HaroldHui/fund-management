398863dec963e39f8fbb546ea49d138dd15693d9
Shu Hui <dreamlock0630@gmail.com>
2019-04-16 23:38:36 +1000

    add date picker

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 const { ipcRenderer } = window.require('electron');
 
 class App extends Component {
-  state = {
-    msg: ''
+  constructor(props) {
+    super(props);
+    this.state = {
+      msg: '',
+      startDate: new Date(),
+      endDate: new Date()
+    };
+    this.handleChangeStart = this.handleChangeStart.bind(this);
+    this.handleChangeEnd = this.handleChangeEnd.bind(this);
   }
+
+  handleChangeStart(date) {
+    this.setState({
+      startDate: date
+    });
+  }
+
+  handleChangeEnd(date) {
+    this.setState({
+      endDate: date
+    });
+  }
+
   onButtonClick() {
     ipcRenderer.send('getReport', 'send message from react');
   }
@@ -21,6 +44,26 @@ class App extends Component {
       <div className="App">
         <button onClick={this.onButtonClick}>Click me</button>
         <p>Message from Server: {this.state.msg}</p>
+
+        <DatePicker
+          selected={this.state.startDate}
+          selectsStart
+          startDate={this.state.startDate}
+          endDate={this.state.endDate}
+          dateFormat="MM/yyyy"
+          showMonthYearPicker
+          onChange={this.handleChangeStart}
+        />
+
+        <DatePicker
+          selected={this.state.endDate}
+          selectsEnd
+          startDate={this.state.startDate}
+          endDate={this.state.endDate}
+          dateFormat="MM/yyyy"
+          showMonthYearPicker
+          onChange={this.handleChangeEnd}
+        />
       </div>
     );
   }
